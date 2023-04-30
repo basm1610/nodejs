11f9976f857b3f03482f59bd071f3211a7c3379f
basm1610 <86631963+basm1610@users.noreply.github.com>
2023-04-30 14:55:43 +0200

    add basic packages in project

---
+++ server.js
@@ -0,0 +1,17 @@
+import express  from "express";
+import mongoose from "mongoose";
+import { engine } from "express-handlebars";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.mongConnection);
+
+const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+
+app.listen(process.env.port,()=>{
+    console.log(`Started application on URL: http://localhost:${process.env.port}`);
+})
